5d85afa2d4a36af1ded8ff47307569171e35aa34
sagic <sagic@wix.com>
2015-04-06 11:07:45 +0300

    a

--- target/view/shared/login.js
+++
@@ -1,8 +0,0 @@
-define(['react', 'lodash', './login.rt'], function (React, _, template) {
-    'use strict';
-
-    return React.createClass({
-        displayName: 'login',
-        render: template
-    });
-});
